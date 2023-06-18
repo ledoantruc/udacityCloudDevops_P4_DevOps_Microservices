f34bdfb232b4faedda43ead168b15701ba9f65fd
ledoantruc <ldtruc93@gmail.com>
2023-06-18 15:52:56 +0700

    add log for prediction

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -60,9 +60,13 @@ def predict():
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
     scaled_payload = scale(inference_payload)
+    LOG.info(f"The scaled payload: {scaled_payload}")
+
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info(f"The prediction value: {prediction}")
+    
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
